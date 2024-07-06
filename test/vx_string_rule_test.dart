c9f3ce1a890f4266365eca6a9fe93946a470239f
Olivier Huin <olih@users.noreply.github.com>
2024-07-06 15:51:15 +0100

    Adds VxWordsMoreThanOrEqualRule test

--- test/vx_string_rule_test.dart
+++ test/vx_string_rule_test.dart
@@ -520,5 +520,75 @@ void main() {
         });
       });
     });
+    group('VxWordsMoreThanOrEqualRule', () {
+      test('validate with producers', () {
+        final rule = VxStringRules.wordsMoreThanOrEqual<String>(
+            'test', metricStoreHolder, 10,
+            successProducer: successProducer, failureProducer: failureProducer);
+        expect(rule.validate({'test-minWords': '3'}, createWords(3)),
+            ['Success: Condition met.']);
+        expect(rule.validate({'test-minWords': '3'}, createWords(2)),
+            ['Failure: Condition not met.']);
+      });
+
+      test('validate without producers', () {
+        final ruleWithoutProducers = VxStringRules.wordsMoreThanOrEqual<String>(
+            'test', metricStoreHolder, 3);
+        expect(ruleWithoutProducers.validate({}, createWords(4)), []);
+        expect(ruleWithoutProducers.validate({}, createWords(2)), []);
+      });
+
+      test('KeyNotFound metric logging', () {
+        final ruleWithoutProducers = VxStringRules.wordsMoreThanOrEqual<String>(
+            'test', metricStoreHolder, 3);
+        ruleWithoutProducers.validate({}, createWords(2));
+
+        final count = ExMetricAggregations.count();
+        final aggregatedMetrics = metricStoreHolder.store.aggregateAll(count);
+
+        expect(aggregatedMetrics.first.toJson(), {
+          'key': {
+            'name': ['get-min-words'],
+            'dimensions': {
+              'package': 'validomix',
+              'class': 'VxWordsMoreThanOrEqualRule',
+              'method': 'validate',
+              'name': 'test',
+              'level': 'ERROR',
+              'status': 'not-found',
+              'unit': 'count',
+              'aggregation': 'count'
+            }
+          },
+          'value': 1.0
+        });
+      });
+      test('KeyInvalid metric logging', () {
+        final ruleWithoutProducers = VxStringRules.wordsMoreThanOrEqual<String>(
+            'test', metricStoreHolder, 3);
+        ruleWithoutProducers
+            .validate({'test-minWords': 'invalid'}, createWords(2));
+
+        final count = ExMetricAggregations.count();
+        final aggregatedMetrics = metricStoreHolder.store.aggregateAll(count);
+
+        expect(aggregatedMetrics.first.toJson(), {
+          'key': {
+            'name': ['get-min-words'],
+            'dimensions': {
+              'package': 'validomix',
+              'class': 'VxWordsMoreThanOrEqualRule',
+              'method': 'validate',
+              'name': 'test',
+              'level': 'ERROR',
+              'error': 'format-exception',
+              'unit': 'count',
+              'aggregation': 'count'
+            }
+          },
+          'value': 1.0
+        });
+      });
+    });
   });
 }
